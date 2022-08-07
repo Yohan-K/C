c87cce9bf035e7b0e5a54199f94519009b29dfe7
yohank <rlawjdgus843@gmail.com>
2022-08-07 21:56:04 +0900

    ADD main.c [02-1 예제 변경해보기] - Encoding 설정

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+void PrintName();
+void PrintInfo();
+
 int main() {
-    printf("Hello, World!\n");
+    //printf("Hello, World!\n");
+    PrintName();
+    PrintInfo();
     return 0;
+}
+
+// 문제 1
+// 본인의 이름을 출력하는 프로그램을 작성. 단, printf 함수는 한 번만 호출해야 한다.
+void PrintName() {
+    printf("김정현\n김 정 현\n김  정  현\n");
+}
+
+// 이름, 주소, 전화번호 출력
+void PrintInfo() {
+    printf("이름: %s", "김정현\n");
+    printf("주소: %s", "경기도 의정부시\n");
+    printf("전화번호: %s", "010 7574 4125\n");
 }
