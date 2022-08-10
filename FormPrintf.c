bd2b220871d6ec686f088026473b736a823bb082
yohank <rlawjdgus843@gmail.com>
2022-08-10 16:49:16 +0900

    ADD FormPrintf.c ADD OctHex.c [정수의 출력을 위한 서식문자들] - 8진수 %#o - 10진수 %d - 16진수 %#x 또는 %x (가급적 서식문자에 '#'을 포함시켜서 출력)

---
+++ FormPrintf.c
@@ -0,0 +1,11 @@
+//
+// Created by kimjeonghyeon on 2022-08-10.
+//
+#include <stdio.h>
+
+int main(void)
+{
+    int myAge=12;
+    printf("제 나이는 10진수로 %d살, 16진수로 %X살입니다. \n", myAge, myAge);
+    return 0;
+}
---
+++ OctHex.c
@@ -0,0 +1,12 @@
+//
+// Created by kimjeonghyeon on 2022-08-10.
+//
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 7, num2 = 13;
+    printf("%o %#o \n", num1, num1); // 8진수 출력
+    printf("%x %#x \n", num2, num2); // 16진수 출력
+    return 0;
+}
