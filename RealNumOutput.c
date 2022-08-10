ec73225519753a1689800e442cf4e2d0dc2a81a6
yohank <rlawjdgus843@gmail.com>
2022-08-10 17:08:56 +0900

    [실수의 출력을 위한 서식문자들 %f, %e, %g] ADD RealNumOutput.c ADD UsingPG.c
    [필드 폭을 지정하여 정돈된 출력 보이기]
    ADD FieldWidth.c

---
+++ RealNumOutput.c
@@ -0,0 +1,13 @@
+//
+// Created by kimjeonghyeon on 2022-08-10.
+//
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%f \n", 0.1234);
+    printf("%e \n", 0.1234); // e 표기법 기반의 출력
+    printf("%f \n", 0.12345678);
+    printf("%e \n", 0.12345678); // e 표기법 기반의 출력
+    return 0;
+}
