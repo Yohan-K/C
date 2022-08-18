07a7594e2818ed0dc76a80bd35fe7661fb8c1a83
yohank <rlawjdgus843@gmail.com>
2022-08-18 16:47:20 +0900

    [포인터 배열] ADD PointerArray.c ADD StringArray.c

---
+++ PointerArray.c
@@ -0,0 +1,15 @@
+//
+// Created by kimjeonghyeon on 2022-08-18.
+//
+#include <stdio.h>
+
+int main(void)
+{
+    int num1=10, num2=20, num3=30;
+    int* arr[3]={&num1, &num2, &num3};
+
+    printf("%d \n", *arr[0]);
+    printf("%d \n", *arr[1]);
+    printf("%d \n", *arr[2]);
+    return 0;
+}
